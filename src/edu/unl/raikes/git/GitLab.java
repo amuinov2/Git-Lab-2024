06bef7953574201ad58a7ace36827eb6bb6921f5
Emmett Myers <emmettleemyers@gmail.com>
2024-02-07 11:07:33 -0600

    Second commit

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -4,6 +4,8 @@ public class GitLab {
 
 	public static void main(String args[]) {
 		System.out.print("This is a lesson on git.");
+		GitLabHelper helper = new GitLabHelper("Name1", "Name2");
+		helper.PrintWelcomeMessage();
 	}
 	
 }
